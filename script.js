7a7bf23e4d37921877daa02635eece1ff3270111
vaishnavitodur123 <vaishnavintodur@gmail.com>
2025-11-26 17:30:33 +0530

    functionality added

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-let msg = document.getElementById("msg");
+let user_score = 0;
+let comp_score = 0;
 
-// msg.innerHTML = "You won!";
+const choices = document.querySelectorAll(".col-1");
+
+const gencomputer = () => {
+    const option = ["rock", "paper", "scissors"];
+    let arr = Math.floor(Math.random() * 3);
+    return option[arr];
+};
+
+const playgame = (userchoices) => {
+    console.log(userchoices);
+
+    const gencomp = gencomputer();
+    console.log(gencomp);
+};
+
+choices.forEach(function (choice) {
+    choice.addEventListener("click", () => {
+        const userchoices = choice.getAttribute("id");
+        // console.log("choice was clicked", userchoices);
+        playgame(userchoices);
+    });
+});
